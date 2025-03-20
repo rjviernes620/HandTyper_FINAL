effe4c273d302aa583621ecf068721d0d7f88e2c
rjviernes620 <rjviernes620@gmail.com>
2025-03-20 17:00:11 +0000

    Begin transferring code from former repo

---
+++ main.py
@@ -0,0 +1,13 @@
+##HandTyperMain - Main Class for the HandTyper Programme
+##Author: Roel-Junior Alejo Viernes (001221190)
+##Email: rv6049z@gre.ac.uk
+
+
+#import Modules
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+import cv2
+import os
